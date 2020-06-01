331312af199458df9297b79383845df4a677bf3a
12youssef <55513240+12youssef@users.noreply.github.com>
2020-06-01 14:24:23 +0100

    nimation

--- resources/views/struct.blade.php
+++ resources/views/struct.blade.php
@@ -21,8 +21,52 @@
 
         <style type="text/css">
             body * {
-                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
-            }
+                font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif; 
+         </style>
+
+<style type="text/css">
+}
+#playArea {
+  background: #999; 
+  height: 20%;
+}
+
+</style>
+
+
+        <style type="text/css">
+          .ml11 {
+  font-weight: 700;
+  font-size: 3.5em;
+}
+
+.ml11 .text-wrapper {
+  position: relative;
+  display: inline-block;
+  padding-top: 0.1em;
+  padding-right: 0.05em;
+  padding-bottom: 0.15em;
+}
+
+.ml11 .line {
+  opacity: 0;
+  position: absolute;
+  left: 0;
+  height: 100%;
+  width: 3px;
+  background-color: #fff;
+  transform-origin: 0 50%;
+}
+
+.ml11 .line1 { 
+  top: 0; 
+  left: 0;
+}
+
+.ml11 .letter {
+  display: inline-block;
+  line-height: 1em;
+}
         </style>
 
 
@@ -75,12 +119,15 @@
         <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
             <div class="modal-dialog" role="document">
                 <div class="modal-content">
-                    <div class="modal-body">Se déconnecter ?</div>
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                        <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        </button>
+                    </div>
+                    <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
                     <div class="modal-footer">
-                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Retour</button>
-                        <a class="btn btn-primary" href="{{ route('logout') }}"
-                            onclick="event.preventDefault();
-                             document.getElementById('logout-form').submit();">Confirmer</a>
+                        <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                        <a class="btn btn-primary" href="login.html">Logout</a>
                     </div>
                 </div>
             </div>
@@ -109,7 +156,228 @@
                 },5000);
             });
         </script>
-        
+
+
+       <script type="text/javascript">
+  // Wrap every letter in a span
+var textWrapper = document.querySelector('.ml11 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/([^\x00-\x80]|\w)/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0, document.querySelector('.ml11 .letters').getBoundingClientRect().width + 10],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: (el, i) => 34 * (i+1)
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
+       </script>
+
+
+
+<script type="text/javascript">
+   window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       ||
+          window.webkitRequestAnimationFrame ||
+          window.mozRequestAnimationFrame    ||
+          function( callback ){
+            window.setTimeout(callback, 1000 / 23);
+          };
+})();
+
+
+$(function(){
+  $(window).resize(function(){
+    $("#playArea").attr({
+      'width': $('body').width(),
+      'height': $('body').height()
+    });
+    
+    width = $('body').width();
+    height = $('body').height();
+    
+  });
+  $(window).resize();
+});
+
+var Point = function(){
+  this._size = 0.5;
+  this._x = 0;
+  this._y = 0;
+  this._direction = 0;
+  this._velocity = 0;
+  this._distances = [];
+  this._neighboors = [];
+  this._randomization = 0;
+
+  this.__collection = null;
+  
+  
+  this._step = function(aCollection){
+
+    var modifiedVelocity = this._velocity*1 * ((Math.random() * this._randomization+1)/10);
+    var direction = (Math.random()*2%2 >1)?-1:1;
+
+    this._direction =  this._direction*1 + ((Math.random() * this._randomization) * direction);
+    var radianAngle = this._direction * Math.PI / 180;
+
+    
+    this._x = (this._x * 1) + (modifiedVelocity * Math.cos(radianAngle) * 1);
+    this._y = (this._y * 1) + (modifiedVelocity * Math.sin(radianAngle) * 1);
+
+    if(this._x > width) this._x = 0;
+    if(this._x < 0) this._x = width;
+    if(this._y > height) this._y = 0;
+    if(this._y < 0) this._y = height;
+ 
+    this.__collection = aCollection;
+    
+
+  }
+  
+  this._computeNeighboors = function() {
+    if(this.__collection == null)  return;
+
+    aCollection = this.__collection;
+    //compute the closest neighboor
+    this._distances = [];
+    for(i=0; i < aCollection.length; i++){
+      
+      if(aCollection[i]._x != this._x &&
+         aCollection[i]._y != this._y){
+
+
+        this._distances.push({
+          pointIndex: i,
+          pointObj: aCollection[i],
+          distance: Math.sqrt( 
+            Math.pow((this._x - aCollection[i]._x), 2) + 
+            Math.pow((this._y - aCollection[i]._y), 2)
+          )
+        });
+      }
+    }
+    this._distances.sort(function(a,b){ 
+      defaultReturn = 0;
+      if(a.distance < b.distance) defaultReturn = -1;
+      if(a.distance > b.distance) defaultReturn = 1;
+      return defaultReturn
+
+    });
+    
+    this._neighboors = this._distances.slice(0,3);
+  }
+
+  
+  this.draw = function(context){
+    this._computeNeighboors();
+    
+    //draw connection lines
+    
+    context.lineWidth = 0.25;
+    context.strokeStyle = '#666';
+    
+    context.beginPath();    
+    for(i=0; i<this._neighboors.length; i++) {
+
+        context.moveTo(this._x, this._y);
+        context.lineTo(this._neighboors[i].pointObj._x,   this._neighboors[i].pointObj._y);
+      
+        context.lineWidth = 0.10 + 5 / this._neighboors[i].distance;
+    }
+    context.closePath();
+
+    context.stroke();
+    
+    context.beginPath();
+    context.arc(this._x, this._y, this._size*this._velocity, 0, 2 * Math.PI, false);
+    context.fillStyle = '#999';
+    context.strokeStyle = '#666';
+    context.lineWidth = 0.5;
+    context.fill();
+    context.stroke();
+    
+    context.beginPath();
+    context.arc(this._x, this._y, this._size, 0, 2 * Math.PI, false);
+    context.fillStyle = '#666';
+    context.fill();
+    
+//    ctx.fillText(this._direction.toFixed(2), this._x, this._y);
+    
+  }
+}
+
+
+var aPoints = [];
+var can, ctx, interval, width, height;
+var numPoints = 50;
+/* var distanceTreshold = 100; */
+
+function init() {
+  can = document.getElementById("playArea");
+  ctx = can.getContext("2d");
+  width = $('body').width();
+  height = $('body').height();
+  
+  for(x=0; x<numPoints; x++){
+    var newPoint = new Point();
+    newPoint._size = (Math.random() * (3 - 0.5) + 0.5).toFixed(2);
+    newPoint._x = (Math.random() * width).toFixed(0);
+    newPoint._y = (Math.random() * height).toFixed(0);
+    newPoint._direction = (Math.random() * 360).toFixed(2);
+    newPoint._velocity = (Math.random() * (4 - 0.1) + 0.2).toFixed(2);
+    newPoint._randomization = (Math.random() * (10 - 0) + 0).toFixed(2);
+    aPoints.push(newPoint);
+  }
+
+  animate();
+}
+
+function animate(){
+  for(x=0; x<numPoints; x++){
+    aPoints[x]._step(aPoints);
+  }
+  requestAnimFrame( animate );
+  draw();
+}
+
+function draw(){
+  ctx.save();
+
+  ctx.clearRect(0,0,width,height);
+  
+  for(x=0; x<numPoints; x++){
+    aPoints[x].draw(ctx);
+  }
+  
+  ctx.restore();
+}
+
+init();
+</script>
+
+
+  
         @yield('scripts')
 
     </body>
